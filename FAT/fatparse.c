3ee62808906866f2ddf20f0b74c14d0a20b4ee48
ttduy <trantrongduy99@gmail.com>
2018-07-14 08:46:48 +0700

    LFN Support, Fixes bugs

--- FAT/fatparse.c
+++ FAT/fatparse.c
@@ -11,5 +11,5 @@ void showPath(DIR *activeDir)
 
 void printDate(uint16_t data_Fat_Format)
 {
-    printf("%hu - %hu - %hu", (data_Fat_Format & 0x001F), ((data_Fat_Format >> 5) & 0x000F), ((data_Fat_Format >> 9) & 0x007F) + 1980);
+    printf("%02hu - %02hu - %hu", (data_Fat_Format & 0x001F), ((data_Fat_Format >> 5) & 0x000F), ((data_Fat_Format >> 9) & 0x007F) + 1980);
 }
--- FAT/main.c
+++ FAT/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
+#include <ctype.h>
 #include <windows.h>
 #include "fatparse.h"
 
@@ -18,6 +19,8 @@ BYTE *g_Buffer = NULL;
 DWORD bytesRead;
 HANDLE g_Device = NULL;
 uint32_t g_SubDirectoryNum = 0;
+char **g_LFN_Array = NULL;
+uint8_t g_LFN_Length = 0;
 
 void showCurrentState();
 char showCurCluster();
@@ -25,12 +28,25 @@ uint32_t fat_Look_Up(uint32_t cluster);
 void moveToCluster(uint32_t cluster);
 uint32_t str_To_Uint32(char *str);
 void handleCommand(uint32_t dirIndex);
+void showEntryInfo(char *entry);
+void showEntryName(char *entry);
+void free_LFN_Array();
+void UCS_2_To_UTF_8(uint16_t character);
 
 int main(int argc, char **argv)
 {
+    SetConsoleOutputCP(65001);      //UTF-8
     char command[MAX_COMMAND_SIZE];
-    g_Buffer = (BYTE *) malloc(512);
-    g_Device = CreateFile("\\\\.\\J:",    // Drive to open
+    command[0] = '\\';
+    command[1] = '\\';
+    command[2] = '.';
+    command[3] = '\\';
+    command[5] = ':';
+    command[6] = 0;
+    printf("Enter a drive letter (of which has been formated with FAT32): ");
+    scanf("%c%*[^\n]", command + 4);
+    getchar();
+    g_Device = CreateFile(command,    // Drive to open
                         GENERIC_READ,           // Access mode
                         FILE_SHARE_READ | FILE_SHARE_WRITE,        // Share Mode
                         NULL,                   // Security Descriptor
@@ -40,10 +56,11 @@ int main(int argc, char **argv)
 
     if(g_Device == INVALID_HANDLE_VALUE)
     {
-        printf("CreateFile: %lu\n", GetLastError());
+        printf("Unable to open drive! Error code: %lu\n", GetLastError());
         getchar();
         return 1;
     }
+    g_Buffer = (BYTE *) malloc(512);
     SetFilePointer (g_Device, 0, NULL, FILE_BEGIN);
     ReadFile(g_Device, g_Buffer, 512, &bytesRead, NULL);
     memmove(&g_Boot_Sector, g_Buffer, 512);
@@ -103,8 +120,6 @@ void showCurrentState()
 
 char showCurCluster()
 {
-    const static char s_Flags[6] = "RHSVDA";
-    static char s_LFN_Check = 0;
     int i;
     for (i = 0; i < CLUSTER_SIZE; i += 32)
     {
@@ -117,54 +132,20 @@ char showCurCluster()
         {
             continue;
         }
-        else if (((g_Buffer[i + 11] >> 2) & 1) || ((g_Buffer[i + 11] >> 3) & 1))
-        {
-            continue;
-        }
         else if (g_Buffer[i + 11] == 0x0F)
         {
+            if ((g_Buffer[i] >> 6) & 1)
+            {
+                g_LFN_Array = (char **) calloc( (g_Buffer[i] & 0x1F), sizeof(char *));
+                g_LFN_Length = (g_Buffer[i] & 0x1F);
+            }
+        }
+        else if (((g_Buffer[i + 11] >> 2) & 1) || ((g_Buffer[i + 11] >> 3) & 1))
+        {
+            free_LFN_Array();
             continue;
-        }                                        
-        for (int j = 0; j < 6; j++)
-        {
-            if ((g_Buffer[i + 11] >> j) & 1)
-            {
-                putchar(s_Flags[j]);
-            }
-            else
-            {
-                putchar('-');
-            }
         }
-        printf("     %-10u", *(uint32_t *) (g_Buffer + i + 28));
-        printf("   ");
-        printDate(*(uint16_t *)(g_Buffer + i + 16));
-        printf("        ");
-        printDate(*(uint16_t *)(g_Buffer + i + 24));
-        printf("         ");
-        for (int j = 0; j < 8; j++)
-        {
-            if (g_Buffer[i + j] != 32)
-            {
-                putchar(g_Buffer[i + j]);
-            }
-        }
-        if (!((g_Buffer[i + 11] >> 4) & 1))
-        {
-            putchar('.');
-        }
-        for (int j = 8; j < 11; j++)
-        {
-            if (g_Buffer[i + j] != 32)
-            {
-                putchar(g_Buffer[i + j]);
-            }
-        }
-        if ((g_Buffer[i + 11] >> 4) & 1)
-        {
-            printf("           -Index: %u", g_SubDirectoryNum - 1);
-        }
-        putchar(10);
+        showEntryInfo(g_Buffer + i);
     }
     return (i < CLUSTER_SIZE);
 }
@@ -242,6 +223,7 @@ void handleCommand(uint32_t dirIndex)
     if (!((g_Buffer[dirIndex + 11] >> 4) & 1) || (g_Buffer[dirIndex] == '.' && g_Buffer[dirIndex + 1] == '.'))
     {
         printf("Invalid index!\n");
+        Sleep(1000);
         return;
     }
     DIR *newSub = (DIR *) malloc(sizeof(DIR));
@@ -249,10 +231,133 @@ void handleCommand(uint32_t dirIndex)
     for (lenFolderName = 0; g_Buffer[dirIndex + lenFolderName] != 32 && lenFolderName < 8; lenFolderName++){};
     newSub->name = (char *) calloc(lenFolderName + 1, 1);
     memmove(newSub->name, g_Buffer + dirIndex, lenFolderName);
-    newSub->cluster = ((*(uint16_t *) (g_Buffer + dirIndex + 20) )<< 16) | (*(uint16_t *) (g_Buffer + dirIndex + 26));
+    newSub->cluster = ( (*(uint16_t *)(g_Buffer + dirIndex + 20)) << 16) | (*(uint16_t *)(g_Buffer + dirIndex + 26));
     newSub->next = g_currentDir;
     g_currentDir = newSub;
     return;
+}
+void showEntryInfo(char *entry)
+{
+    const static char s_Flags[6] = "RHSVDA";
+    if (entry[11] == 0x0F && g_LFN_Array != NULL)
+    {
+        uint8_t index = (entry[0] & 0x1F) - 1;
+        g_LFN_Array[index] = (char *) malloc(26);
+        memmove(g_LFN_Array[index], entry + 1, 10);
+        memmove(g_LFN_Array[index] + 10, entry + 14, 12);
+        memmove(g_LFN_Array[index] + 22, entry + 28, 4);
+        return;
+    }
+    for (int j = 0; j < 6; j++)
+    {
+        if ((entry[11] >> j) & 1)
+        {
+            putchar(s_Flags[j]);
+        }
+        else
+        {
+            putchar('-');
+        }
+    }
+    printf("     %-10u", *(uint32_t *) (entry + 28));
+    printf("   ");
+    printDate(*(uint16_t *)(entry + 16));
+    printf("       ");
+    printDate(*(uint16_t *)(entry + 24));
+    printf("        ");
+    showEntryName( (g_LFN_Array == NULL) ? (entry) : (NULL));
+    if ((entry[11] >> 4) & 1)
+    {
+        printf("           -Index: %u", g_SubDirectoryNum - 1);
+    }
+    putchar(10);
+}
+
+void showEntryName(char *entry)
+{
+    if (entry == NULL)
+    {
+        for (uint8_t i = 0; i < g_LFN_Length; i++)
+        {
+            for (uint8_t j = 0; j < 26; j += 2)
+            {
+                if (g_LFN_Array[i][j] == 0)
+                {
+                    break;
+                }
+                UCS_2_To_UTF_8( *(uint16_t *) (g_LFN_Array[i] + j) );
+            }
+        }
+        free_LFN_Array();
+        return;
+    }
+
+    for (int j = 0; j < 8; j++)
+    {
+        if (entry[j] != 32)
+        {
+            putchar( (((entry[12] >> 3) & 1) ? (tolower(entry[j])) : entry[j]));
+        }
+    }
+    if (!(((entry[11] >> 4) & 1) || (entry[8] + entry[9] + entry[10] == 96)))
+    {
+        putchar('.');
+    }
+    for (int j = 8; j < 11; j++)
+    {
+        if (entry[j] != 32)
+        {
+            putchar( (((entry[12] >> 4) & 1) ? (tolower(entry[j])) : entry[j]));
+        }
+    }
+}
+
+void free_LFN_Array()
+{
+    for (uint8_t i = 0; i < g_LFN_Length; i++)
+    {
+        free(g_LFN_Array[i]);
+    }
+    free(g_LFN_Array);
+    g_LFN_Array = NULL;
+    g_LFN_Length = 0;
+}
+
+void UCS_2_To_UTF_8(uint16_t character)
+{
+    if (character == 0)
+    {
+        return;
+    }
+    int8_t magnitude = 16;
+    char *str = NULL;
+    while (magnitude--)
+    {
+        if (character >> magnitude)
+        {
+            break;
+        }
+    }
+    magnitude++;
+    if (magnitude < 8)
+    {
+        str = (char *) calloc(2, 1);
+        str[0] = character;
+    }
+    else if (magnitude < 12)
+    {
+        str = (char *) calloc(3, 1);
+        str[0] = (0xC0 | (character >> 6));
+        str[1] = (0x80 | (character & 0x3F));
+    }
+    else
+    {
+        str = (char *) calloc(4, 1);
+        str[0] = (0xE0 | (character >> 12));
+        str[1] = (0x80 | ((character >> 6) & 0x3F));
+        str[2] = (0x80 | (character & 0x3F));
+    }
+    printf("%s", str);
 }
 // typedef struct
 // {
